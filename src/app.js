a13f7d3b42eba1412dd93dc08644212404219f02
ahmadali333 <ahmadali.cali8@gmail.com>
2026-01-02 13:08:49 -0400

    fixed the cors issue

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ app.use(
       'http://localhost:5173',
       'http://localhost:5174',
       'https://forrof-tracker.vercel.app',
+      'https://tracker.forrof.io'
     ],
     credentials: true,
   })
